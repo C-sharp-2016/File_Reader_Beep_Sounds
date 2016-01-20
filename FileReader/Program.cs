dac9ddee55d3971c72dfb0771317832ee97b4253
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-20 08:00:22 +0800

    read file, read each file content, split, frequency and duration

--- FileReader/Program.cs
+++ FileReader/Program.cs
@@ -11,7 +11,22 @@ namespace FileReader
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            StreamReader sr = new StreamReader("../../../songs.txt");
+
+            string data = sr.ReadLine();
+
+            while(data != null)
+            {
+                Console.WriteLine(data);
+                data = sr.ReadLine();
+
+                string[] values = data.Split(',');
+                int frequency = Int32.Parse(values[0]);
+                int duration = Int32.Parse(values[1]); 
+                Console.Beep(frequency, duration);
+            }  
+            //Console.WriteLine("This is just a test"); 
 
         }
     }
